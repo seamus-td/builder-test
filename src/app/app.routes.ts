3b9740c175f06606afd0d58f870064d7f649d1b5
seamus-td <seamus.sutula@terradotta.com>
2025-08-20 13:10:35 -0400

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,4 @@
 import { Routes } from "@angular/router";
-import { ProgramCardComponent } from "./components/program-card/program-card.component";
 
 export const routes: Routes = [
-  { path: "", component: ProgramCardComponent },
-  { path: "find-programs", component: ProgramCardComponent },
 ];
